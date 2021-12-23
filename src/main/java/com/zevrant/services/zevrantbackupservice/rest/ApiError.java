cac9ba820e739e1b74070cd19f6485f6383ed634
zevrant <workmailtome@gmail.com>
2021-12-23 09:38:27 -0500

    added generic exception handler

---
+++ src/main/java/com/zevrant/services/zevrantbackupservice/rest/ApiError.java
@@ -0,0 +1,44 @@
+package com.zevrant.services.zevrantbackupservice.rest;
+
+public class ApiError {
+
+    private String errorMessage;
+    private String exceptionClass;
+    private int responseStatus;
+    private String stackTrace;
+
+    public ApiError() {
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getExceptionClass() {
+        return exceptionClass;
+    }
+
+    public void setExceptionClass(String exceptionClass) {
+        this.exceptionClass = exceptionClass;
+    }
+
+    public int getResponseStatus() {
+        return responseStatus;
+    }
+
+    public void setResponseStatus(int responseStatus) {
+        this.responseStatus = responseStatus;
+    }
+
+    public String getStackTrace() {
+        return stackTrace;
+    }
+
+    public void setStackTrace(String stackTrace) {
+        this.stackTrace = stackTrace;
+    }
+}
