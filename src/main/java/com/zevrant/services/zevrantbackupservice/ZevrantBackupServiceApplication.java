2442be2937add5ccde0fc3c553ba1842159aec3e
zevrant <workmailtome@gmail.com>
2022-01-09 17:57:23 -0500

    added logging for memory allocation

--- src/main/java/com/zevrant/services/zevrantbackupservice/ZevrantBackupServiceApplication.java
+++ src/main/java/com/zevrant/services/zevrantbackupservice/ZevrantBackupServiceApplication.java
@@ -1,5 +1,7 @@
 package com.zevrant.services.zevrantbackupservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -7,8 +9,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication(scanBasePackages = {"com.zevrant.services"})
 @EnableTransactionManagement
 public class ZevrantBackupServiceApplication {
-
+    private static final Logger logger = LoggerFactory.getLogger(ZevrantBackupServiceApplication.class);
     public static void main(String[] args) {
+        logger.info("Total memory is: {}GB", Runtime.getRuntime().maxMemory() / Math.Math.pow(10, -9));
         SpringApplication.run(ZevrantBackupServiceApplication.class, args);
     }
 
